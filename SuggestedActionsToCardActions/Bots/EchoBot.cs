8c7623b409b5b81675b59871b945c0933752236a
cpjoshi <chandraprakashjoshi@gmail.com>
2020-03-29 20:40:13 +0530

    Workaround: Convert Suggested Actions to AdaptiveCards in Microsft Teams

---
+++ SuggestedActionsToCardActions/Bots/EchoBot.cs
@@ -0,0 +1,160 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using SuggestedActionsToCardActions.Utils;
+
+namespace SuggestedActionsToCardActions.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        bool bReplaceSuggestedActions = true;
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Text("Choose one of the following");
+            switch (turnContext.Activity.Text)
+            {
+                case "Start":
+                    {
+                        Activity welcome = WelcomeMessage();
+                        await turnContext.SendActivityAsync(welcome, cancellationToken, bReplaceSuggestedActions);
+                        return;
+                    }
+                case "ViewMyProfile":
+                    {
+                        IMessageActivity cardReply = GetProfileCard();
+                        cardReply.SuggestedActions = new SuggestedActions()
+                        {
+                            Actions = new List<CardAction>()
+                            {
+                                new CardAction() {Title = "Home", Type=ActionTypes.ImBack, Value="Home"},
+                                new CardAction() {Title = "Cancel", Type=ActionTypes.ImBack, Value="Cancel"}
+                            }
+                        };
+
+                        await turnContext.SendActivityAsync(cardReply, cancellationToken, bReplaceSuggestedActions);
+                        return;
+                    }
+
+                case "EmployeeProfile":
+                    {
+                        reply.SuggestedActions = new SuggestedActions()
+                        {
+                            Actions = new List<CardAction>()
+                            {
+                                new CardAction() { Title = "View My Profile", Type = ActionTypes.ImBack, Value = "ViewMyProfile" },
+                                new CardAction() { Title = "Create Profile", Type = ActionTypes.ImBack, Value = "CreateProfile" },
+                                new CardAction() { Title = "Update Profile", Type = ActionTypes.ImBack, Value = "UpdateProfile" },
+                                new CardAction() { Title = "Delete Profile", Type = ActionTypes.ImBack, Value = "DeleteProfile" },
+                            },
+                        };
+                    }
+                    break;
+
+                case "ApplyLeave":
+                    {
+                        reply.SuggestedActions = new SuggestedActions()
+                        {
+                            Actions = new List<CardAction>()
+                            {
+                                new CardAction() { Title = "Maternity Leave", Type = ActionTypes.ImBack, Value = "MaternityLeave" },
+                                new CardAction() { Title = "Paternity Leave", Type = ActionTypes.ImBack, Value = "PaternityLeave" },
+                                new CardAction() { Title = "Sick Leave", Type = ActionTypes.ImBack, Value = "SickLeave" },
+                            },
+                        };
+                    }
+                    break;
+
+                case "SeePaySlip":
+                    {
+                        reply.SuggestedActions = new SuggestedActions()
+                        {
+                            Actions = new List<CardAction>()
+                            {
+                                new CardAction() { Title = "For this month", Type = ActionTypes.ImBack, Value = "Forthismonth" },
+                                new CardAction() { Title = "For last month", Type = ActionTypes.ImBack, Value = "Forlastmonth" },
+                            },
+                        };
+                    }
+                    break;
+
+                default:
+                    reply = MessageFactory.Text("End", "End");
+                    break;
+            }
+            await turnContext.SendActivityAsync(reply, cancellationToken, bReplaceSuggestedActions);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            Activity reply = WelcomeMessage();
+
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(reply, cancellationToken, bReplaceSuggestedActions);
+                }
+            }
+        }
+
+        private static Activity WelcomeMessage()
+        {
+            var reply = MessageFactory.Text("Hello and welcome, choose one of the actions below?");
+
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    new CardAction() { Title = "Employee Profile", Type = ActionTypes.ImBack, Value = "EmployeeProfile" },
+                    new CardAction() { Title = "Apply Leave", Type = ActionTypes.ImBack, Value = "ApplyLeave" },
+                    new CardAction() { Title = "See Payslip", Type = ActionTypes.ImBack, Value = "SeePayslip" },
+                },
+            };
+            return reply;
+        }
+
+        private static IMessageActivity GetProfileCard()
+        {
+            var card = new HeroCard()
+            {
+                Title = "Your Profile - Card with suggested actions",
+                Subtitle = "Name: C P Joshi",
+                Buttons = new List<CardAction>
+                            {
+                                new CardAction
+                                {
+                                    Type= ActionTypes.MessageBack,
+                                    Title = "One",
+                                    Text = "One",
+                                },
+                                new CardAction
+                                {
+                                    Type= ActionTypes.MessageBack,
+                                    Title = "Two",
+                                    Text = "Two",
+                                },
+                                new CardAction
+                                {
+                                    Type= ActionTypes.MessageBack,
+                                    Title = "Three",
+                                    Text = "Three",
+                                }
+                            }
+            };
+
+            var cardReply = MessageFactory.Attachment(card.ToAttachment());
+            return cardReply;
+        }
+
+
+
+    }
+}
---
+++ SuggestedActionsToCardActions/Utils/TurnContextExtensions.cs
@@ -0,0 +1,66 @@
+﻿using AdaptiveCards;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuggestedActionsToCardActions.Utils
+{
+    public static class TurnContextExtensions
+    {
+        private static string PreviousSuggestedActionActivityId = string.Empty;
+
+        public async static Task<ResourceResponse> SendActivityAsync(
+            this ITurnContext turnContext,
+            IMessageActivity activity, 
+            CancellationToken cancellationToken = default,
+            bool bReplaceSuggestedActions = false)
+        {
+            //webchat does not support updateactivity
+            if(turnContext.Activity.ChannelId == "webchat" || 
+                activity.SuggestedActions == null || 
+                !bReplaceSuggestedActions)
+            {
+                PreviousSuggestedActionActivityId = string.Empty;
+                return await turnContext.SendActivityAsync(activity, cancellationToken);
+            }
+
+            //This is to clear the UI for the previous suggested actions
+            //await turnContext.ClearLastActivity(cancellationToken);
+
+            //Create a new card having suggested action buttons
+            var card = new HeroCard() { 
+                Buttons = activity.SuggestedActions.Actions 
+            };
+
+            //Remove suggested actions from origianl activity
+            activity.SuggestedActions = null;
+
+            //send origianl activity
+            await turnContext.SendActivityAsync(activity, cancellationToken);
+
+            //send suggested actions card
+            var response = await turnContext.SendActivityAsync(
+                MessageFactory.Attachment(card.ToAttachment()), 
+                cancellationToken);
+
+            PreviousSuggestedActionActivityId = response.Id;
+            return response;
+        }
+
+        public async static Task<ResourceResponse> ClearLastActivity (
+            this ITurnContext turnContext,
+            CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrEmpty(PreviousSuggestedActionActivityId))
+                return null;
+
+            var blankActivity = MessageFactory.Text("Clear");
+            blankActivity.Id = PreviousSuggestedActionActivityId;
+            return await turnContext.UpdateActivityAsync(blankActivity, cancellationToken);
+        }
+    }
+}
